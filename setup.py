56922b135d9c822b9c084a4861d1be39a9d7724d
mhagdorn <magnus.hagdorn@marsupium.org>
2020-03-26 14:12:55 +0000

    make flake8 happy

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "taskfarm-worker",
-    packages = find_packages(),
-    include_package_data = True,
-    install_requires = [
-	'requests',
+    name="taskfarm-worker",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
     ],
     entry_points={
         'console_scripts': [
-            'manageTF = taskfarm_worker.manage:main',
+            'manageTF=taskfarm_worker.manage:main',
         ],
     },
-    author = "Magnus Hagdorn",
-    description = "worker module for taskfarm",
+    author="Magnus Hagdorn",
+    description="worker module for taskfarm",
 )
