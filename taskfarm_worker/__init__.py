d097c3ab73a8677cb049fcc26db50b382715bb00
mhagdorn <magnus.hagdorn@marsupium.org>
2020-03-31 13:26:23 +0100

    get version from setup script

--- taskfarm_worker/__init__.py
+++ taskfarm_worker/__init__.py
@@ -1,1 +1,8 @@
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution('taskfarm-worker').version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
 from .taskfarm import *
